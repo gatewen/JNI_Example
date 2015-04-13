9b5d2c7678b94a860de4601dae6de96ad68ecb60
gatewen <gatewen@gmail.com>
2015-04-13 15:55:17 +0800

    1.修正 UtilTools.h 無法編譯的問題 2.加入 .gitgnore檔案

--- Classes/UtilTools.h
+++ Classes/UtilTools.h
@@ -12,22 +12,9 @@ class UtilTools: public Singleton<UtilTools>
     {
 public:
     bool init();
-	template<class T>
     long long getNowUnixTime();	//取得目前時間
     long long getTomorrowZeroTime();	//取得明日零點時間
     long long getTomorrowExcessTime();	//距離明日剩餘時間
 
-    };
-
-template<class T>
-std::string UtilTools::NumberToString(T value)
-{
-	std::stringstream ss;
-	std::string temp;
-	ss << value;
-	ss >> temp;
-	return temp;
-}
-	
-	
+    };	
 #endif
